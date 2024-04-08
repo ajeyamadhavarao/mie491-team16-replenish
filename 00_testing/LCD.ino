8c316e6bf9b302ca2cb26f5899d7ad61d99d5e11
parinj05 <66892409+parinj05@users.noreply.github.com>
2024-04-07 23:45:29 -0400

    LCD I2C and image display codes

--- 00_testing/LCD.ino
+++ 00_testing/LCD.ino
@@ -1,84 +1,81 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <TFT_eSPI.h> // Hardware-specific library
+#include <Adafruit_GFX.h>
+#include <MCUFRIEND_kbv.h>
+#include <Wire.h>
 
-TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+MCUFRIEND_kbv tft;
 
-#define TFT_GREY 0x5AEB // New colour
+#define LCD_CS A3
+#define LCD_RS A2
+#define LCD_WR A1
+#define LCD_RD A0
+#define LCD_RST A4
 
-#define TFT_DC   18  // Data Command control pin - must use a pin in the range 0-31  (LCD_RS on my display)
-#define TFT_CS   33  // Chip select control pin
-#define TFT_RST  32  // Reset pin
-#define TFT_RD    2
-#define TFT_WR    4  // Write strobe control pin - must use a pin in the range 0-31
+#define SCREEN_WIDTH  480
+#define SCREEN_HEIGHT 320
 
-#define TFT_D0   12  // Must use pins in the range 0-31 for the data bus
-#define TFT_D1   13  // so a single register write sets/clears all bits
-#define TFT_D2   26
-#define TFT_D3   25
-#define TFT_D4   21
-#define TFT_D5   5
-#define TFT_D6   27
-#define TFT_D7   14
+#define GREEN 0x44be44
+#define GREY 0x3c444c
 
-#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
-#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
-#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
-#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
-#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:.
-#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
-#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+#define SLAVE_ADDRESS 9 // Define the I2C slave address of the Arduino Uno
 
-#define SMOOTH_FONT
+bool displayed = false;
+
+// Liquid types
+const char* liquidTypes[] = {"Engine Oil", "Dish Soap", "Detergent"};
+int liquidInput = -1; 
 
 void setup() {
-  tft.init();
-  tft.setRotation(1);
+  
+  Serial.println("Setup BEFORE I2C");
+  Wire.begin(SLAVE_ADDRESS); // Initialize as I2C slave with address 9
+  Wire.onReceive(receiveEvent); // Set up callback function for received data
+  Serial.begin(9600); // Initialize Serial communication
 }
 
 void loop() {
-  // Fill screen with random colour so we can see the effect of printing with and without 
-  // a background colour defined
-  tft.fillScreen(random(0xFFFF));
-  
-  // Set "cursor" at top left corner of display (0,0) and select font 2
-  // (cursor will move to next line automatically during printing with 'tft.println'
-  //  or stay on the line is there is room for the text with tft.print)
-  tft.setCursor(0, 0, 2);
-  // Set the font colour to be white with a black background, set text size multiplier to 1
-  tft.setTextColor(TFT_WHITE,TFT_BLACK);  tft.setTextSize(1);
-  // We can now plot text on screen using the "print" class
-  // tft.println("Hello World!");
-  
-  // Set the font colour to be yellow with no background, set to font 7
-  tft.setTextColor(TFT_YELLOW); tft.setTextFont(7);
-  tft.println(1234.56);
-  
-  // Set the font colour to be red with black background, set to font 4
-  tft.setTextColor(TFT_RED,TFT_BLACK);    tft.setTextFont(4);
-  tft.println((long)3735928559, HEX); // Should print DEADBEEF
+  // No delay here to avoid automatically showing "Engine Oil"
+}
 
-  // Set the font colour to be green with black background, set to font 4
-  tft.setTextColor(TFT_GREEN,TFT_BLACK);
-  tft.setTextFont(4);
-  tft.println("REPLENISH");
-  // tft.println("I implore thee,");
-
-  // Change to font 2
-  tft.setTextFont(2);
-  // tft.println(F("my foonting turlingdromes.")); // Can store strings in FLASH to save RAM
-  // tft.println("And hooptiously drangle me");
-  // tft.println("with crinkly bindlewurdles,");
-  // This next line is deliberately made too long for the display width to test
-  // automatic text wrapping onto the next line
-  tft.println("Or I will rend thee in the gobberwarts with my blurglecruncheon, see if I don't!");
+void receiveEvent(int bytes) {
+  Serial.println("Inside receiveEVENT");
   
-  // Test some print formatting functions
-  float fnumber = 123.45;
-   // Set the font colour to be blue with no background, set to font 4
-  tft.setTextColor(TFT_BLUE);    tft.setTextFont(4);
-  tft.print("Float = "); tft.println(fnumber);           // Print floating point number
-  tft.print("Binary = "); tft.println((int)fnumber, BIN); // Print as integer value in binary
-  tft.print("Hexadecimal = "); tft.println((int)fnumber, HEX); // Print as integer number in Hexadecimal
-  delay(10000);
+  int c = Wire.read(); // Read the incoming byte
+  Serial.println("Received: " + String(c));
+  liquidInput = c;
+  Serial.println(liquidTypes[liquidInput]);
+  displayLiquidType(liquidInput);
+ 
+}
+
+void displayLiquidType(int input) {
+  // Clear previous liquid type display
+  // tft.fillRect(0, SCREEN_HEIGHT / 2 - 20, SCREEN_WIDTH, 40, GREY);
+
+  uint16_t ID = 0x9486; // ILI9486 ID
+  tft.reset(); // Initialize display
+  tft.begin(ID);
+  tft.setRotation(1);
+
+  // Set background color to grey
+  tft.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GREY);
+
+  // Set text color and size
+  tft.setTextColor(0xFFFF); // White color
+  tft.setTextSize(5);
+  if (input >= 0 && input < sizeof(liquidTypes) / sizeof(liquidTypes[0])) {
+    // Calculate text width to center horizontally
+    int16_t x, y;
+    uint16_t textWidth, textHeight;
+    tft.getTextBounds(liquidTypes[input], 0, 0, &x, &y, &textWidth, &textHeight);
+    Serial.println("Inside displayLiquidType: "); 
+    Serial.print(liquidTypes[input]);
+    // Display liquid type string horizontally
+    tft.setCursor((SCREEN_WIDTH - textWidth) / 2, (SCREEN_HEIGHT - textHeight) / 2);
+    tft.print(liquidTypes[input]);
+  } else {
+    // Display error message for invalid input
+    tft.setTextColor(GREEN); // Green color
+    tft.setCursor(SCREEN_WIDTH / 2 - 60, SCREEN_HEIGHT / 2 - 10);
+    tft.println("Invalid Input");
+  }
 }
