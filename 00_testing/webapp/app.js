ff2515cc87e3b92ff48eca77f3ce62c7199743d2
ajeyamadhavarao <ajeyamadhavarao@gmail.com>
2024-04-11 03:12:29 -0400

    code maybe ready for showcase, MVP worked

--- 00_testing/webapp/app.js
+++ 00_testing/webapp/app.js
@@ -1,6 +1,5 @@
-// Your web app's Firebase configuration (replace this with your project's config object)
+// Your web app's Firebase configuration
 var firebaseConfig = {
-
     apiKey: "AIzaSyBqpEv0CFXZ1mjvodzJqX1AsCsJUNAkn9Q",
     authDomain: "replenish-team16.firebaseapp.com",
     databaseURL: "https://replenish-team16-default-rtdb.firebaseio.com",
@@ -8,59 +7,61 @@ var firebaseConfig = {
     storageBucket: "replenish-team16.appspot.com",
     messagingSenderId: "326923385198",
     appId: "1:326923385198:web:80e7f7b194f7f5e0fe697f"
-
 };
-  
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Reference your database location
+// Reference to your database
 var database = firebase.database();
 
-// Function to write data
-function writeData1() {
-    var data1 = document.getElementById('dataInput1').value;
-    var intValue1 = parseInt(data1); // Make sure to convert string to integer
-  
-    // Using set() will overwrite data at the location
-    database.ref('pump/left/').update({intValue1});
-  
-    // If you want to create a unique entry with an integer, use push() like this:
-    // database.ref('data/').push(intValue);
-    document.getElementById('dataInput1').value = ''; // Clear input after submit
+// Function to handle the toggle switch change
+function handleToggle(toggleId, path) {
+  var checkbox = document.getElementById(toggleId);
+  var slider = checkbox.nextElementSibling; // Assuming slider is right after checkbox
+
+  if (checkbox.checked) {
+    // Add 'on' class to make it visibly turned on
+    slider.classList.add('on');
+    // Update the database path with value 1 to indicate on state
+    database.ref(path).update({value: 1});
+  } else {
+    // This part will execute if the checkbox is clicked when already checked
+    slider.classList.remove('on');
+    // Update the database with value 0 to indicate off state
+    database.ref(path).update({value: 0});
+  }
 }
 
-function writeData2() {
-    var data2 = document.getElementById('dataInput2').value;
-    var intValue2 = parseInt(data2); // Make sure to convert string to integer
-  
-    // Using set() will overwrite data at the location
-    database.ref('pump/middle/').update({intValue2});
-  
-    // If you want to create a unique entry with an integer, use push() like this:
-    // database.ref('data/').push(intValue);
-    document.getElementById('dataInput2').value = ''; // Clear input after submit
-}
-function writeData3() {
-    var data3 = document.getElementById('dataInput3').value;
-    var intValue3 = parseInt(data3); // Make sure to convert string to integer
-  
-    // Using set() will overwrite data at the location
-    database.ref('pump/right/').update({intValue3});
-  
-    // If you want to create a unique entry with an integer, use push() like this:
-    // database.ref('data/').push(intValue);
-    document.getElementById('dataInput3').value = ''; // Clear input after submit
-}
+// Function to dispense and reset all toggles
 function writeData4() {
-    var data4 = document.getElementById('dataInput4').value;
-    var intValue4 = parseInt(data4); // Make sure to convert string to integer
-  
-    // Using set() will overwrite data at the location
-    database.ref('pump/vol/').update({intValue4});
-  
-    // If you want to create a unique entry with an integer, use push() like this:
-    // database.ref('data/').push(intValue);
-    document.getElementById('dataInput4').value = ''; // Clear input after submit
+  var volumeInput = document.getElementById('dataInput4');
+  var value = parseInt(volumeInput.value);
+
+  database.ref('pump/vol/').update({value});
+  volumeInput.value = '';
+
+  // Reset all toggles and remove 'on' class to revert to the normal state
+  ['toggle1', 'toggle2', 'toggle3'].forEach(function(toggleId) {
+    var checkbox = document.getElementById(toggleId);
+    var slider = checkbox.nextElementSibling;
+    checkbox.checked = false;
+    slider.classList.remove('on');
+  });
 }
 
+// Event listeners for the toggle switches
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('toggle1').addEventListener('change', function() {
+    handleToggle('toggle1', 'pump/left/');
+  });
+  document.getElementById('toggle2').addEventListener('change', function() {
+    handleToggle('toggle2', 'pump/middle/');
+  });
+  document.getElementById('toggle3').addEventListener('change', function() {
+    handleToggle('toggle3', 'pump/right/');
+  });
+  
+  // Attach event listener to the dispense button to reset toggles after dispensing
+  document.getElementById('dispenseButton').addEventListener('click', writeData4);
+});
